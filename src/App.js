01c3f000c863754dba2443136ee6121afb10a1e9
lakshitha <sailakshitha409@gmail.com>
2023-01-30 13:10:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css'
+import { faker } from '@faker-js/faker';
+
+function Card(){
+  return(<div >
+    <div class="extra content">
+      <div class="ui two buttons">
+        <div class="ui basic green button">Approve</div>
+        <div class="ui basic red button">Decline</div>
+      </div>
+    </div></div>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="ui cards">
+  <div class="card">
+    <div class="content">
+      <img class="right floated mini ui image" src={faker.image.nature()}/>
+      <div class="header">
+        Elliot Fu
+      </div>
+      <div class="meta">
+        Friends of Veronika
+      </div>
+      <div class="description">
+        Elliot requested permission to view your contact details
+      </div>
     </div>
+    <button className='button'><Card/></button>
+  </div>
+  
+  <div class="card">
+    <div class="content">
+      <img class="right floated mini ui image" src={faker.image.nature()}/>
+      <div class="header">
+        Jenny Hess
+      </div>
+      <div class="meta">
+        New Member
+      </div>
+      <div class="description">
+        Jenny wants to add you to the group <b>best friends</b>
+      </div>
+    </div><button className='button'><Card/></button>
+</div>
+</div>
   );
 }
 
-export default App;
+
+export {App,Card};
